415d2317cadbc8080baa1471db812f2882f4738c
Greg Lim <limjunqu@gmail.com>
2020-05-13 08:05:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import UserForm from './UserForm';
+import GitHub from './GitHub';
 
 function App() {
   return (
     <div>
-        <UserForm />
+        <GitHub />
     </div>
   );
 }
